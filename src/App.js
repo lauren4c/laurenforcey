01960331f7a6eecafd1fdb2f90ac048e7b3a1f12
Lauren Forcey <lauren4c@gmail.com>
2019-01-29 09:18:37 -0700

    Created website via React

--- src/App.js
+++ src/App.js
@@ -1,28 +1,56 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  getYear() {
+    return new Date().getFullYear();
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+        <div className="purple-blue">
+          <div className="header-text">
+            <h1>Hey There!</h1>
+            <h2>
+              My name is Lauren. I am a designer and developer.
+              <br />
+              Let's create something great together.
+            </h2>
+          </div>
+        </div>
+        <div className="meantime">
+          <div className="icons">
+            <a href="https://www.linkedin.com/in/lauren4c/">
+              <ion-icon name="logo-linkedin" />
+            </a>
+            <a href="https://github.com/lauren4c">
+              <ion-icon name="logo-github" />
+            </a>
+            <a href="http://www.coroflot.com/lauren4c">
+              <ion-icon name="brush" />
+            </a>
+            <a href="https://4cblocjams.netlify.com/">
+              <ion-icon name="code-working" />
+            </a>
+            <a href="mailto:lauren4c@gmail.com">
+              <ion-icon name="send" />
+            </a>
+          </div>
+        </div>
+        <div className="footer">
+          <p className="footer-text">
+            Hand-coded in Summit County, Colorado. ©{this.getYear()} Lauren
+            Forcey
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        </div>
       </div>
     );
   }
 }
 
 export default App;
+// <p>
+//   In the meantime, if you need any design assistance or help with
+//   your // small business website, //{" "}
+//   <a href="mailto:lauren4c@gmail.com">send Lauren an email.</a>{" "}
